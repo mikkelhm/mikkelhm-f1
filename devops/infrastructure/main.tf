8861da44237ff673ebc3d0b2f42e9a3d77d921e3
mikkelhm <mikkel@h-madsen.dk>
2022-07-28 09:25:55 +0200

    rtfm!

--- devops/infrastructure/main.tf
+++ devops/infrastructure/main.tf
@@ -96,7 +96,7 @@ resource "azurerm_linux_function_app" "fa-functions-mikkelhm-f1" {
   location            = azurerm_resource_group.rg-mikkelhm-f1.location
 
   storage_account_name       = azurerm_storage_account.sa-functions-mikkelhm-f1.name
-  storage_account_access_key = azurerm_storage_account.sa-functions-mikkelhm-f1.storage_account_access_key
+  storage_account_access_key = azurerm_storage_account.sa-functions-mikkelhm-f1.primary_access_key
   service_plan_id            = azurerm_service_plan.ap-functions-mikkelhm-f1.id
 
   site_config {
