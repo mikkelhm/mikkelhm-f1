b736c801c5fe5135b478de975478d769659b5428
mikkelhm <mikkel@h-madsen.dk>
2022-10-19 09:03:44 +0200

    fix screwup

--- devops/infrastructure/providers.tf
+++ devops/infrastructure/providers.tf
@@ -26,7 +26,7 @@ provider "azurerm" {
   tenant_id       = var.azure_tenant_id
   subscription_id = var.azure_subscription_id
   client_id       = var.azure_client_id
-  client_secret   = var.
+  client_secret   = var.azure_client_secret
   features {}
 }
 
