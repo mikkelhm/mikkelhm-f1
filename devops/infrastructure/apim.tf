c4800337f4565f789e9ec7da6ee01521d930956e
mikkelhm <mikkel@h-madsen.dk>
2023-01-02 23:22:21 +0100

    fmt

--- devops/infrastructure/apim.tf
+++ devops/infrastructure/apim.tf
@@ -41,5 +41,5 @@ resource "azurerm_api_management_subscription" "f1_api_subscription" {
   api_management_name = azurerm_api_management.apim.name
   resource_group_name = azurerm_api_management.apim.resource_group_name
   display_name        = "f1_api_subscription"
-  api_id              = azurerm_api_management_api.f1_api.id 
+  api_id              = azurerm_api_management_api.f1_api.id
 }
