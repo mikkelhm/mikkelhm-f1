f0a78310fa39f6b196fb9ea714221f37351ad5a6
mikkelhm <mikkel@h-madsen.dk>
2023-01-03 09:05:15 +0100

    Revert "kill the api"
    This reverts commit 5dd2a08427ac5b08579589ea45145829e9e23ca9.

--- devops/infrastructure/apim.tf
+++ devops/infrastructure/apim.tf
@@ -21,57 +21,57 @@ resource "cloudflare_record" "apim_cname_record" {
   ttl     = 1 # Cloudflare will terminate TLS
 }
 
-# resource "azurerm_api_management_api" "f1_api" {
-#   name                = "f1"
-#   resource_group_name = azurerm_resource_group.rg-mikkelhm-f1.name
-#   api_management_name = azurerm_api_management.apim.name
-#   revision            = "1"
-#   display_name        = "F1 API"
-#   path                = "f1"
-#   protocols           = ["https"]
-#   service_url         = "https://${azurerm_linux_function_app.fa-functions-api-mikkelhm-f1.default_hostname}/api/"
+resource "azurerm_api_management_api" "f1_api" {
+  name                = "f1"
+  resource_group_name = azurerm_resource_group.rg-mikkelhm-f1.name
+  api_management_name = azurerm_api_management.apim.name
+  revision            = "1"
+  display_name        = "F1 API"
+  path                = "f1"
+  protocols           = ["https"]
+  service_url         = "https://${azurerm_linux_function_app.fa-functions-api-mikkelhm-f1.default_hostname}/api/"
 
-#   import {
-#     content_format = "swagger-link-json"
-#     content_value  = "https://${azurerm_linux_function_app.fa-functions-api-mikkelhm-f1.default_hostname}/api/swagger.json"
-#   }
-# }
+  import {
+    content_format = "swagger-link-json"
+    content_value  = "https://${azurerm_linux_function_app.fa-functions-api-mikkelhm-f1.default_hostname}/api/swagger.json"
+  }
+}
 
-# resource "azurerm_api_management_api_policy" "f1_api_policy" {
-#   api_name            = azurerm_api_management_api.f1_api.name
-#   api_management_name = azurerm_api_management.apim.name
-#   resource_group_name = azurerm_api_management.apim.resource_group_name
-#   xml_content         = <<XML
-# <policies>
-#     <inbound>
-#         <base />
-#         <cors allow-credentials="false">
-#             <allowed-origins>
-#                 <origin>*</origin>
-#             </allowed-origins>
-#             <allowed-methods>
-#                 <method>GET</method>
-#                 <method>POST</method>
-#             </allowed-methods>
-#         </cors>
-#     </inbound>
-#     <backend>
-#         <base />
-#     </backend>
-#     <outbound>
-#         <base />
-#     </outbound>
-#     <on-error>
-#         <base />
-#     </on-error>
-# </policies>
-#   XML
-# }
+resource "azurerm_api_management_api_policy" "f1_api_policy" {
+  api_name            = azurerm_api_management_api.f1_api.name
+  api_management_name = azurerm_api_management.apim.name
+  resource_group_name = azurerm_api_management.apim.resource_group_name
+  xml_content         = <<XML
+<policies>
+    <inbound>
+        <base />
+        <cors allow-credentials="false">
+            <allowed-origins>
+                <origin>*</origin>
+            </allowed-origins>
+            <allowed-methods>
+                <method>GET</method>
+                <method>POST</method>
+            </allowed-methods>
+        </cors>
+    </inbound>
+    <backend>
+        <base />
+    </backend>
+    <outbound>
+        <base />
+    </outbound>
+    <on-error>
+        <base />
+    </on-error>
+</policies>
+  XML
+}
 
-# resource "azurerm_api_management_subscription" "f1_api_subscription" {
-#   api_management_name = azurerm_api_management.apim.name
-#   resource_group_name = azurerm_api_management.apim.resource_group_name
-#   display_name        = "f1_api_subscription"
-#   api_id              = azurerm_api_management_api.f1_api.id
-#   state               = "active"
-# }
+resource "azurerm_api_management_subscription" "f1_api_subscription" {
+  api_management_name = azurerm_api_management.apim.name
+  resource_group_name = azurerm_api_management.apim.resource_group_name
+  display_name        = "f1_api_subscription"
+  api_id              = azurerm_api_management_api.f1_api.id
+  state               = "active"
+}
